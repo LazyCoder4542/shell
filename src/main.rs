dbde52d9b58c86103afab09ab57716cddb79cb7b
Adeola Fadare <adeolafadare8@gmail.com>
2026-07-31 11:28:03 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self, Write, Read};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    let mut input = String::new();
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let input = input.trim();
+
+    println!("{}: command not found", input);
+
+    let _ = std::io::stdin().read(&mut [0u8]).unwrap();
 }
